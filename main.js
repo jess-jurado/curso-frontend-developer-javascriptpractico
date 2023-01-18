3c598d0f0f767f2f5dfd8434e441e1e664bf6b08
Jess <jssgjurado@gmail.com>
2023-01-18 22:55:41 +0100

    añadido el menu izquierdo de la parte mobile

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
+
+// Constantes
+
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const logoMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const logoCarrito = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail');
+
+
+// Eventos
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+logoMenu.addEventListener('click', toggleMobileMenu);
+logoCarrito.addEventListener('click', toggleCartMenu);
+
+
+// Funciones
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+    console.log("click");
+}
+ 
+function toggleCartMenu() {
+    productDetail.classList.toggle('inactive');
+    console.log("clock");
 }
